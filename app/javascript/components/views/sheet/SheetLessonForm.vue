9bec4ac98d4f9caaee284721086e1e0b81123607
root <root@HP-ENVY-309.localdomain>
2021-06-22 13:38:00 -0400

    Intermediate commit

--- app/javascript/components/views/sheet/SheetLessonForm.vue
+++ app/javascript/components/views/sheet/SheetLessonForm.vue
@@ -13,6 +13,33 @@ export default {
   components: {
     LessonForm,
   },
+  mixins: [
+    LessonForm,
+  ],
+  beforeRouteLeave(to, from, next) {
+    if (this.lesson !== this.DV_lesson) {
+      this.$confirm(
+        `Are you sure you want to close without saving?`, "Leave Site Without Saving?",
+        {
+          confirmButtonText: "Save",
+          cancelButtonText: "Don't Save",
+          type: "warning",
+        }
+      )
+        .then(() => {
+          this.saveLesson()
+          next();
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "Changes not saved.",
+            showClose: true,
+          });
+          next();
+        });
+    }
+  },
 };
 </script>
 
